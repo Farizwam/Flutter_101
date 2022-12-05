b08f3725802b44601cdbc7108a80eee730285934
Farizwam <118846858+Farizwam@users.noreply.github.com>
2022-12-05 23:48:58 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("Welcome to flutter mobile application development");
+
+  int number1 = 6, number2 = 4;
+  int number3 = (number1 + number2);
+  print(number3);
+  //print(number1 + number2);
+  //print(number1 - number2);
+  //print(number1 * number2);
+  //double number11 = 3.2, number12 = 4.5, number13 = 6.5;
+  //print(number11 * number12 * number13);
+}
