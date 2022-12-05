d78f97d64a23f47c2f36b804c35467c1928383f7
Farizwam <118846858+Farizwam@users.noreply.github.com>
2022-12-05 23:32:37 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+void main() {
+//The distance between two cities (in km.) is input through the
+//keyboard. Write a program to convert and print this distance
+//in meters, feet, inches and centimeters.
+//in Meters
+  print("enter the distance in km:");
+  int distance = int.parse(stdin.readLineSync()!);
+  print(distance * 1000);
+  // in Feet
+  print(distance * 3280.89);
+  print(distance * 39370.1);
+  print(distance * 100000);
+  //If the marks obtained by a student in five different subjects
+//are input through the keyboard, find out the aggregate marks
+//and percentage marks obtained by the student. Assume that
+//the maximum marks that can be obtained by a student in each
+//subject is 100.
+  print("enter english marks:");
+  int english = int.parse(stdin.readLineSync()!);
+  print("enter math marks:");
+  double math = double.parse(stdin.readLineSync()!);
+  print("enter urdu marks:");
+  int urdu = int.parse(stdin.readLineSync()!);
+  print("enter sstudies marks:");
+  int sstudies = int.parse(stdin.readLineSync()!);
+  print("enter computer marks:");
+  double computer = double.parse(stdin.readLineSync()!);
+  double aggregatemarks = english + math + urdu + sstudies + computer;
+  print(aggregatemarks / 500);
+  //percetage
+  print(aggregatemarks / 500 * 100);
+  //Temperature of a city in Fahrenheit degrees is input through
+//the keyboard. Write a program to convert this temperature
+//into Centigrade degrees.
+  print("enter the temperature in Fahrenheit:");
+  int temp = int.parse(stdin.readLineSync()!);
+  print((temp - 32) * .5556);
+  //The length & breadth of a rectangle and radius of a circle are
+//input through the keyboard. Write a program to calculate the
+//area & perimeter of the rectangle, and the area &
+//circumference of the circle
+  print("length of a rectangle:");
+  double length = double.parse(stdin.readLineSync()!);
+  print("breadth of a rectangle:");
+  int breadth = int.parse(stdin.readLineSync()!);
+  print(length * breadth);
+  print((2 * length) + (2 * breadth));
+  print("enter the area of circle:");
+  int area = int.parse(stdin.readLineSync()!);
+  //area of a circle
+  print(3.14 * area);
+// circumference of a circle
+  print(2 * 3.14 * area);
+}
